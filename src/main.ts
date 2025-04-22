7a244de9052e56378511ae0b2882fb967417a8c9
bytesbanana <saharat.paynok@gmail.com>
2025-04-22 23:57:39 +0700

    feat: add healthcheck API endpoint

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,14 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
+app.get('/api/healthcheck', (c) => {
+  return c.json({ status: 'OK' })
+})
+
 serve({
   fetch: app.fetch,
   port: 3000
 }, (info) => {
   console.log(`Server is running on http://localhost:${info.port}`)
 })
+
