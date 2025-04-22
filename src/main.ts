47b2d33ab281b3b9bbf87ab292a1286804c5f74e
bytesbanana <saharat.paynok@gmail.com>
2025-04-23 00:11:23 +0700

    feat: add ESLint configuration and linting script

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,22 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-app.get('/api/healthcheck', (c) => {
-  return c.json({ status: 'OK' })
-})
+app.get("/api/healthcheck", (c) => {
+  return c.json({ status: "OK" });
+});
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
-
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  },
+);
